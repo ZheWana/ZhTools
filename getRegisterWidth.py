5b3e6ab11759fbf05b5c92ec4bd5bda3bd5b38e6
zhewana@qq.com <zhewana@qq.com>
2024-10-15 13:49:52 +0800

    file: 添加计算寄存器宽度工具

---
+++ getRegisterWidth.py
@@ -0,0 +1,10 @@
+# 功能：输入寄存器需要存储数值的最大值，获取对应寄存器的位数。
+
+max_value = int(input("Input max value: "))
+reg_width = 0
+while True:
+    if 2**reg_width - 1 >= max_value:
+        print(f"Register width: {reg_width}, with max value: {2**reg_width - 1}")
+        break
+    else:
+        reg_width += 1
