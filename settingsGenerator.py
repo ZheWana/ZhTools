53bb3ba1f67dce121583e24fc9925cd187fb020c
zhewana@qq.com <zhewana@qq.com>
2024-11-13 22:04:17 +0800

    完善settings生成脚本

--- settingsGenerator.py
+++ settingsGenerator.py
@@ -4,7 +4,7 @@
 # 2.若工作区内含有多个Keil工程，询问使用的工程，没有的话将预定义宏留空
 # 3.若所选工程内含有多个目标，询问使用的目标
 # 4.询问用户可能使用的编译器路径以及具体的可执行文件
-# 
+#
 # 注意：
 # 脚本使用覆盖式生成，使用前请先备份原settings.json文件避免数据丢失
 
@@ -17,9 +17,10 @@ compilerPath = ""
 preDefines = []
 includePath = []
 
+
 # 将列表内容展示给用户并返回用户的选项
 def getUserChoice(items: list, discription: str):
-    print("-"*20 + "\nThere are", len(items), discription, ":")
+    print("-" * 20 + "\nThere are", len(items), discription, ":")
     for i, item in enumerate(items):
         print(i, ":", item)
     num = input("Please input a integer to select one:")
@@ -34,18 +35,30 @@ def getUserChoice(items: list, discription: str):
         except:
             print("Wrong input, default to use 0.")
             return 0
-    
+
     return num
 
 
+def isCompilerPath(path: str):
+    CompilerPathKeyWords = ["gcc", "g++", "arm", "iar", "devkit"]
+    lowerPath = path.lower()
+    for word in CompilerPathKeyWords:
+        if word in lowerPath:
+            return True
+
+    return False
+
+
 # 询问工作区编码
-temp = input("Please set encoding for workspace(u for utf-8, g for gb2312, others will directly fill into the setting)")
+temp = input(
+    "Please set encoding for workspace(u for utf-8, g for gb2312, others will directly fill into the setting)"
+)
 if temp == "u" or temp == "":
-    encoding = "\"files.encoding\": \"utf8\","
+    encoding = '"files.encoding": "utf8",'
 elif temp == "g":
-    encoding = "\"files.encoding\": \"gb2312\","
+    encoding = '"files.encoding": "gb2312",'
 else:
-    encoding = "\"files.encoding\": \"" + str(temp) + "\","
+    encoding = '"files.encoding": "' + str(temp) + '",'
 
 # 查找后缀为uvprojx或uvproj的文件
 mdkFilePath = ""
@@ -63,7 +76,7 @@ if len(tarList) == 0:
 else:
     if len(tarList) > 1:
         num = getUserChoice(tarList, "projects")
-    
+
     num = 0 if num == "" else num
 
     mdkFilePath = tarList[num]
@@ -74,15 +87,15 @@ else:
         tar = ""
 
         for line in f:
-            target_matches = re.match(r'<TargetName>(.*)</TargetName>', line.strip())
-            define_matches = re.match(r'<Define>(.*)</Define>', line.strip())
+            target_matches = re.match(r"<TargetName>(.*)</TargetName>", line.strip())
+            define_matches = re.match(r"<Define>(.*)</Define>", line.strip())
 
             if tar == "" and target_matches:
                 tar = target_matches.group(1).strip()
 
             if define_matches is not None:
                 if tar != "":
-                    if(tar not in tarList):
+                    if tar not in tarList:
                         tarList[tar] = re.split(",| ", define_matches.group(1).strip())
                     tar = ""
 
@@ -91,9 +104,9 @@ else:
 
 # 生成宏定义
 tarList = [] if num == "" else tarList[list(tarList.keys())[num]]
-preDefines.append("\"C_Cpp.default.defines\": [\n")
+preDefines.append('"C_Cpp.default.defines": [\n')
 for define in tarList:
-    preDefines.append("    \"" + define + "\",\n")
+    preDefines.append('    "' + define + '",\n')
 preDefines.append("],\n")
 
 # 默认脚本所在的目录为工作区根目录，将所有含有.h文件的目录加入列表
@@ -105,25 +118,34 @@ for root, dirs, files in os.walk(".\\"):
             break
 
 # 生成包含路径
-includePath.append("\"C_Cpp.default.includePath\": [\n")
+includePath.append('"C_Cpp.default.includePath": [\n')
 for path in tarList:
-    includePath.append("    \"" + path + "\",\n")
-includePath.append("    \"${workspaceFolder}/**\"\n")
+    includePath.append('    "' + path + '",\n')
+includePath.append('    "${workspaceFolder}/**"\n')
 includePath.append("],\n")
 
 # 查找环境变量中可能的编译器路径(含有bin的路径)，让用户选择
-tarList = list(filter(lambda x: x.endswith("\\bin") or x.endswith("\\Bin"), os.environ.get("Path").split(";")))
+tarList = list(
+    filter(
+        isCompilerPath,
+        os.environ.get("Path").split(";"),
+    )
+)
 num = getUserChoice(tarList, "bin Pathes")
 if num != "":
     # 筛选出所选路径中所有的可执行文件
     for root, dirs, files in os.walk(tarList[num]):
-        tarList = list(filter(lambda x: x.endswith(".exe"), files))
+        tarList = list(
+            filter(lambda x: "gcc" in x or "armcc" in x or "armclang" in x, files)
+        )
         num = getUserChoice(tarList, "programes")
         if num != "":
             compilerPath = os.path.join(root, tarList[num])
         break
 if compilerPath != "":
-    compilerPath = "\"C_Cpp.default.compilerPath\": \"" + compilerPath.replace("\\", "/") + "\","
+    compilerPath = (
+        '"C_Cpp.default.compilerPath": "' + compilerPath.replace("\\", "/") + '",'
+    )
 
 # 创建并将编码、预定义宏、包含路径写入文件中
 if not os.path.exists("./.vscode"):
